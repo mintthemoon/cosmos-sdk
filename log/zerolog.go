08fd2b77c247527785c3e7a8474dc14ed949120f
Julien Robert <julien@rbrt.fr>
2023-02-13 12:18:05 +0100

    feat: improve logger

--- log/zerolog.go
+++ log/zerolog.go
@@ -14,7 +14,7 @@ const ModuleKey = "module"
 var (
 	// ContextKey is used to store the logger in the context
 	ContextKey struct{}
-	_          Logger = (*ZeroLogWrapper)(nil)
+	_          Logger = (*CometZerologWrapper)(nil)
 )
 
 func NewZeroLogger(key, value string) *zerolog.Logger {
@@ -25,38 +25,38 @@ func NewZeroLogger(key, value string) *zerolog.Logger {
 
 // TODO: add filtered logging in ZeroLog: https://github.com/cosmos/cosmos-sdk/pull/13236 / https://github.com/cosmos/cosmos-sdk/issues/13699#issuecomment-1354887644
 
-// ZeroLogWrapper provides a wrapper around a zerolog.Logger instance. It implements
-// Tendermint's Logger interface.
-type ZeroLogWrapper struct {
+// CometZerologWrapper provides a wrapper around a zerolog.Logger instance. It implements
+// CometBFT's Logger interface.
+type CometZerologWrapper struct {
 	zerolog.Logger
 }
 
-// Info implements Tendermint's Logger interface and logs with level INFO. A set
+// Info implements CometBFT's Logger interface and logs with level INFO. A set
 // of key/value tuples may be provided to add context to the log. The number of
 // tuples must be even and the key of the tuple must be a string.
-func (z ZeroLogWrapper) Info(msg string, keyVals ...interface{}) {
+func (z CometZerologWrapper) Info(msg string, keyVals ...interface{}) {
 	z.Logger.Info().Fields(getLogFields(keyVals...)).Msg(msg)
 }
 
-// Error implements Tendermint's Logger interface and logs with level ERR. A set
+// Error implements CometBFT's Logger interface and logs with level ERR. A set
 // of key/value tuples may be provided to add context to the log. The number of
 // tuples must be even and the key of the tuple must be a string.
-func (z ZeroLogWrapper) Error(msg string, keyVals ...interface{}) {
+func (z CometZerologWrapper) Error(msg string, keyVals ...interface{}) {
 	z.Logger.Error().Fields(getLogFields(keyVals...)).Msg(msg)
 }
 
-// Debug implements Tendermint's Logger interface and logs with level DEBUG. A set
+// Debug implements CometBFT's Logger interface and logs with level DEBUG. A set
 // of key/value tuples may be provided to add context to the log. The number of
 // tuples must be even and the key of the tuple must be a string.
-func (z ZeroLogWrapper) Debug(msg string, keyVals ...interface{}) {
+func (z CometZerologWrapper) Debug(msg string, keyVals ...interface{}) {
 	z.Logger.Debug().Fields(getLogFields(keyVals...)).Msg(msg)
 }
 
 // With returns a new wrapped logger with additional context provided by a set
 // of key/value tuples. The number of tuples must be even and the key of the
 // tuple must be a string.
-func (z ZeroLogWrapper) With(keyVals ...interface{}) cmtlog.Logger {
-	return ZeroLogWrapper{z.Logger.With().Fields(getLogFields(keyVals...)).Logger()}
+func (z CometZerologWrapper) With(keyVals ...interface{}) cmtlog.Logger {
+	return CometZerologWrapper{z.Logger.With().Fields(getLogFields(keyVals...)).Logger()}
 }
 
 func getLogFields(keyVals ...interface{}) map[string]interface{} {
